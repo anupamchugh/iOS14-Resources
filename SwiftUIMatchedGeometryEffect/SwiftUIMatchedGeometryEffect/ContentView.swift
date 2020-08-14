0d7085f8ec372ecd34e99cca06defbc0aebe38ad
Anupam Chugh <anupam.chugh@gmail.com>
2020-08-15 01:02:48 +0530

    Update ContentView.swift

--- SwiftUIMatchedGeometryEffect/SwiftUIMatchedGeometryEffect/ContentView.swift
+++ SwiftUIMatchedGeometryEffect/SwiftUIMatchedGeometryEffect/ContentView.swift
@@ -9,89 +9,71 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @Namespace private var namespace
-    @State private var selectedAlbumIDs: Set<Album.ID> = []
-
+    
+    @State var myData = Array(1...10).map{"Item \($0)"}
+    @Namespace var namespace
+    
+    @State private var selectedItemIDs: Set<String> = []
+    var columns = Array(repeating: GridItem(.flexible(), spacing: 10), count: 3)
+    
     var body: some View {
-        VStack(spacing: 0) {
-            ScrollView {
-                albumGrid.padding(.horizontal)
+        VStack {
+            ScrollView{
+                LazyVGrid(columns: columns, alignment: .center, spacing: 20){
+                    ForEach(unselectedItems, id: \.self) { album in
+                       Button(action: { select(album) }) {
+                          ItemCell(album, 100).background(Color.green)
+                            .cornerRadius(6.0)
+                       }
+                       .matchedGeometryEffect(id: album, in: namespace)
+                    }
+                }.padding()
             }
-
-            Divider().zIndex(-1)
-
-            selectedAlbumRow
-                .frame(height: AlbumCell.albumSize)
-                .padding(.top, 8)
-        }
-        .buttonStyle(PlainButtonStyle())
-    }
-
-    private var albumGrid: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: AlbumCell.albumSize))], spacing: 8) {
-           ForEach(unselectedAlbums) { album in
-              Button(action: { select(album) }) {
-                 AlbumCell(album)
-              }
-              .matchedGeometryEffect(id: album.id, in: namespace)
-           }
+            ScrollView(.horizontal){
+                HStack {
+                    ForEach(selectedItems, id: \.self) { item in
+                        Button(action: { deselect(item) }) {
+                           ItemCell(item, 50).background(Color.orange)
+                        }
+                        .matchedGeometryEffect(id: item, in: namespace)
+                    }
+                }
+            }.frame(height: 100)
         }
     }
-
-    private var selectedAlbumRow: some View {
-        HStack {
-            ForEach(selectedAlbums) { album in
-                AlbumCell(album)
-                .matchedGeometryEffect(id: album.id, in: namespace)
-            }
-        }
-    }
-
-    private var unselectedAlbums: [Album] {
-        Album.allAlbums.filter { !selectedAlbumIDs.contains($0.id) }
-    }
-    private var selectedAlbums: [Album] {
-        Album.allAlbums.filter { selectedAlbumIDs.contains($0.id) }
-    }
-
-    private func select(_ album: Album) {
+    
+    private var selectedItems: [String] { myData.filter { selectedItemIDs.contains($0) } }
+    
+    private var unselectedItems: [String] {myData.filter { !selectedItemIDs.contains($0) } }
+    
+    private func select(_ item: String) {
         withAnimation(.spring(response: 0.5)) {
-            _ = selectedAlbumIDs.insert(album.id)
+            _ = selectedItemIDs.insert(item)
+        }
+    }
+    
+    private func deselect(_ item: String) {
+        withAnimation(.spring(response: 0.5)) {
+            _ = selectedItemIDs.remove(item)
         }
     }
 }
 
-struct AlbumCell: View {
-    static let albumSize: CGFloat = 100
+struct ItemCell: View {
+    var itemSize: CGFloat
 
-    var album: Album
+    var item: String
 
-    init(_ album: Album) {
-        self.album = album
+    init(_ item: String, _ itemSize: CGFloat) {
+        self.item = item
+        self.itemSize = itemSize
     }
 
     var body: some View {
-        album.image
-            .frame(width: AlbumCell.albumSize, height: AlbumCell.albumSize)
-            .background(Color.pink)
-            .cornerRadius(6.0)
+            Text(item)
+            .foregroundColor(.white)
+            .frame(width: itemSize, height: itemSize)
     }
-}
-
-struct Album: Identifiable {
-    static let allAlbums: [Album] = [
-        .init(name: "Sample", image: Image(systemName: "music.note")),
-        .init(name: "Sample 2", image: Image(systemName: "music.note.list")),
-        .init(name: "Sample 3", image: Image(systemName: "music.quarternote.3")),
-        .init(name: "Sample 4", image: Image(systemName: "music.mic")),
-        .init(name: "Sample 5", image: Image(systemName: "music.note.house")),
-        .init(name: "Sample 6", image: Image(systemName: "tv.music.note"))
-    ]
-
-    var name: String
-    var image: Image
-
-    var id: String { name }
 }
 
 
